55a01ddc98eb785b2e3788f23e4c077ed8ccaeab
Cameron Murtagh <cameron.murtagh00@gmail.com>
2025-05-19 13:00:35 +0800

    dap commands

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -48,6 +48,17 @@ return {
       dap.listeners.before.event_exited["dap-view-config"] = function()
         dap_view.close()
       end
+
+      vim.keymap.set("n", "<leader>b", function() dap.toggle_breakpoint() end, { desc = "Toggle breakpoint" })
+      vim.api.nvim_create_user_command("DapRunLast", function() dap.run_last() end,
+        { nargs = 0, desc = "Run last debug config" })
+      vim.api.nvim_create_user_command("DapSetBreakpoint", function(args) dap.set_breakpoint(args.fargs[1]) end,
+        { nargs = "?", desc = "Set breakpoint with condition" })
+
+      vim.fn.sign_define('DapBreakpoint', { text = '', texthl = '', linehl = '', numhl = '' })
+      vim.fn.sign_define('DapBreakpointCondition', { text = '', texthl = '', linehl = '', numhl = '' })
+      vim.fn.sign_define('DapLogPoint', { text = '', texthl = '', linehl = '', numhl = '' })
+      vim.fn.sign_define('DapBreakpointRejected', { text = '', texthl = '', linehl = '', numhl = '' })
     end
   },
 }
