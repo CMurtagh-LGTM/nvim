0e3b569a1c9e816c67bcf48e937ad891027d733e
cameron <cameron.murtagh00@gmail.com>
2025-05-24 14:00:08 +1000

    gitsigns

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -314,7 +314,7 @@ return {
   {
     "bassamsdata/namu.nvim",
     keys = {
-      { "<leader>dd", "<cmd>Namu symbols<cr>", mode = "n", desc = "Jump to LSP symbol" },
+      { "<leader>dd", "<cmd>Namu symbols<cr>",   mode = "n", desc = "Jump to LSP symbol" },
       { "<leader>dD", "<cmd>Namu workspace<cr>", mode = "n", desc = "LSP Symbols - Workspace" },
     },
     config = function()
@@ -329,5 +329,74 @@ return {
         },
       })
     end,
+  },
+
+  {
+    "lewis6991/gitsigns.nvim",
+    cond = function ()
+      return #vim.fs.find(".git", { upward = true }) > 0
+    end,
+    opts = {
+      on_attach = function(bufnr)
+        local gitsigns = require('gitsigns')
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', ']c', function()
+          if vim.wo.diff then
+            vim.cmd.normal({ ']c', bang = true })
+          else
+            gitsigns.nav_hunk('next')
+          end
+        end)
+
+        map('n', '[c', function()
+          if vim.wo.diff then
+            vim.cmd.normal({ '[c', bang = true })
+          else
+            gitsigns.nav_hunk('prev')
+          end
+        end)
+
+        -- Actions
+        map('n', '<leader>hs', gitsigns.stage_hunk, {desc = "stage hunk"})
+        map('n', '<leader>hr', gitsigns.reset_hunk, {desc = "reset hunk"})
+
+        map('v', '<leader>hs', function()
+          gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+        end, {desc = "stage hunk"})
+
+        map('v', '<leader>hr', function()
+          gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+        end, {desc = "reset hunk"})
+
+        map('n', '<leader>hS', gitsigns.stage_buffer, {desc = "stage buffer"})
+        map('n', '<leader>hR', gitsigns.reset_buffer, {desc = "reset buffer"})
+        map('n', '<leader>hp', gitsigns.preview_hunk, {desc = "preview hunk"})
+        map('n', '<leader>hi', gitsigns.preview_hunk_inline, {desc = "preview hunk inline"})
+
+        map('n', '<leader>hb', function()
+          gitsigns.blame_line({ full = true })
+        end, {desc = "blame line"})
+
+        map('n', '<leader>hd', gitsigns.diffthis, {desc = "diff this"})
+
+        map('n', '<leader>hD', function()
+          gitsigns.diffthis('~')
+        end, {desc = "digg this against ~"})
+
+        -- Toggles
+        map('n', '<leader>tb', gitsigns.toggle_current_line_blame, {desc = "toggle current line blame"})
+        map('n', '<leader>tw', gitsigns.toggle_word_diff, {desc = "toggle word diff"})
+
+        -- Text object
+        map({ 'o', 'x' }, 'ih', gitsigns.select_hunk, {desc = "select hunk"})
+      end
+    },
   }
 }
