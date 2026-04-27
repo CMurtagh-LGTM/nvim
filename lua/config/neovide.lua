36e3312131b83a00cc8d835f6ea0be8e9c589597
cameron <cameron.murtagh00@gmail.com>
2026-04-27 14:03:51 +1000

    neovide faster animations

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -1,5 +1,7 @@
 -- Neovide config
-vim.g.neovide_cursor_animation_length = 0.1
+vim.g.neovide_cursor_animation_length = 0.05
+vim.g.neovide_position_animation_length = 0
 vim.g.neovide_cursor_trail_size = 0.05
+vim.g.neovide_scroll_animation_far_lines = 0
 
 vim.o.guifont = "monospace:h12"
